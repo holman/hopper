0e605471af4c383864614c09ac2a5902566a366d
Zach Holman <zach@zachholman.com>
2012-02-11 23:57:54 -0800

    TomDoc

--- app/probe.rb
+++ app/probe.rb
@@ -15,15 +15,21 @@ module Hopper
       @path = File.expand_path(path)
     end
 
+    # The name of the probe, generated from the file name.
+    #
+    # Returns a String.
     def name
       File.basename(__FILE__, '.rb').capitalize
     end
 
+    # The description of the probe.
+    #
+    # Returns a String.
     def description
       raise NotImplementedError
     end
 
-    class NotImplementedError < StandardError
-    end
+    # Raised if the method hasn't been properly defined in the subclass.
+    class NotImplementedError < StandardError ; end
   end
 end

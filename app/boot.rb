f906fed83905577f17836b3d18af943db8fd503f
Zach Holman <zach@zachholman.com>
2012-03-10 22:11:24 +0000

    Some commnets

--- app/boot.rb
+++ app/boot.rb
@@ -2,18 +2,23 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 
 require 'rubygems'
 
+# Dependencies
+
 require 'redis'
 require 'sinatra/base'
 require 'mustache/sinatra'
 
+# App
 require 'app'
 require 'app/views/layout'
 
+# Models
 require 'models/index'
 require 'models/probe'
 require 'models/project'
 require 'models/source'
 
+# Extensions
 require 'lib/ext/array'
 
 $redis = Redis.connect(:url => 'redis://127.0.0.1', :thread_safe => true)

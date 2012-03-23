c4303a1e40b92e05ab668cbef46512c85c2dcffc
Zach Holman <zach@zachholman.com>
2012-03-23 16:28:56 -0400

    Days old, commits per day

--- app/probes/commits.rb
+++ app/probes/commits.rb
@@ -2,7 +2,7 @@ module Hopper
   # Analysis of the commits that go into this repo.
   class Commits < Probe
     # The data for this Probe.
-    exposes :total_count
+    exposes :total_count, :commits_per_day, :days_old
 
     # The description.
     #
@@ -18,12 +18,26 @@ module Hopper
       `cd #{project.path} && git rev-list --all | wc -l`.strip.to_i
     end
 
+    # Commits per day metric.
+    #
+    # Returns a Float.
+    def commits_per_day
+      total_count.to_f / days_old
+    end
+
     # The day this repo was created.
     #
     # Returns a DateTime.
     def birthday
       date = `git log --reverse --pretty=format:%ad'' | head -1`.strip
       DateTime.parse(date)
+    end
+
+    # The number of days old this project is.
+    #
+    # Returns an Integer.
+    def days_old(to=Date.today)
+      to - Date.parse(birthday.strftime('%Y-%m-%d'))
     end
   end
 end
--- test/probes/commits_test.rb
+++ test/probes/commits_test.rb
@@ -15,4 +15,19 @@ context "Commits" do
   test "birthday" do
     assert_equal 2, @probe.birthday.month
   end
+
+  test "commits_per_day" do
+    @probe.stubs(:days_old).returns(10)
+    @probe.stubs(:total_count).returns(50)
+    assert_equal 5, @probe.commits_per_day
+  end
+
+  test "days_old" do
+    from = Date.parse('2012-01-15')
+      to = Date.parse('2012-01-18')
+
+    @probe.stubs(:birthday).returns(from)
+
+    assert_equal 3, @probe.days_old(to)
+  end
 end

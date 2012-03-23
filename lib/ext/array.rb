2fb15ccd93babbea9a57a520fb21209933297ab7
Zach Holman <zach@zachholman.com>
2012-03-23 16:06:03 -0400

    Array#average

--- lib/ext/array.rb
+++ lib/ext/array.rb
@@ -2,10 +2,22 @@ class Array
   # Sums up an array.
   #
   # Examples:
-  #   [1,3,6].sum #=> 10
+  #   [1,3,6].sum # => 10
   #
   # Returns an Integer.
   def sum
     self.inject(0) { |sum,i| sum + i }
+  end
+
+  # Average out the values of a collection of Arrays. The array gets flattened,
+  # so you can safely toss it a stack of Arrays.
+  #
+  # Examples:
+  #   [[2,4],[6],8] # => 5
+  #
+  # Returns a Float.
+  def average
+    self.flatten!
+    self.sum / self.size
   end
 end
--- test/unit/array_test.rb
+++ test/unit/array_test.rb
@@ -4,4 +4,9 @@ context "Array" do
   test "sum" do
     assert_equal 10, [1,3,6].sum
   end
+
+  test "average" do
+    assert_equal  5, [2,4,6,8].average
+    assert_equal 20, [10,[20],[20],[[30]]].average
+  end
 end

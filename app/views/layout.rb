0157f6a67876d1ac8b23ca62707a03451ef9a1d5
Zach Holman <zach@zachholman.com>
2012-04-06 21:36:28 -0700

    secondaries

--- app/views/layout.rb
+++ app/views/layout.rb
@@ -4,6 +4,13 @@ module Hopper
       def title
         "Hopper"
       end
+
+      # We have a secondary nav under the main nav. This always looks for
+      # secondaries/page_name.mustache.
+      def secondaries
+        file = self.class.name.split('::').last.gsub!(/(.)([A-Z])/,'\1_\2').downcase
+        Mustache.render(File.read("app/templates/secondaries/#{file}.mustache"))
+      end
     end
   end
 end
